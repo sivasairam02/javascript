2f4c40dcab93aa8f5dc02f2400d634471432fe15
sivasairam02 <sivasairampakkurthi@gmail.com>
2023-08-18 13:56:10 +0530

    first commit

---
+++ external.js
@@ -0,0 +1,14 @@
+// var a=10;
+// var b=20;
+// var c=30;
+// var d=40;
+
+//       	document.write("this is ips</br>",a);
+//       	alert(b);
+//       	console.log(c);
+//       	document.write( " </br>this is shiva",d);
+
+ var name= 'shiva';
+ document.write(name,"<br>");
+ name = 'ips';
+ document.write(name,"<br>");     	
